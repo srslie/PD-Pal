2f9e35c745e3c9e3e00f7844777304f615119d69
srslie <aliceruppert@gmail.com>
2021-03-06 16:58:57 -0700

    Add value bottom border, add user name

--- src/components/Values/Values.js
+++ src/components/Values/Values.js
@@ -6,7 +6,7 @@ export default function Values({user, values}) {
   return(
     <div className='values'>
       <span className='value-text'>
-      <h2 className='values-title'>Job Values</h2>
+      <h2 className='values-title'>{user} Values</h2>
       <p className='values-info'>{values}</p>
       <Link to='/account'>
         <button className='update-values-button'>
