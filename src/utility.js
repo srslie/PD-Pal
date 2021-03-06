d66e778f69ad85d90be07bee7a0157eb1767dcdc
srslie <aliceruppert@gmail.com>
2021-03-06 10:26:26 -0700

    Make api more dynamic, outline mulitple searches

--- src/utility.js
+++ src/utility.js
@@ -1,8 +1,16 @@
 import { defaultFieldResolver } from "graphql";
 
 const utility = {
-  getData() {
-    return fetch('https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=1a060a0c&app_key=4867681585b99bffc654ca5c4768c20c&results_per_page=20&what=javascript%20developer&content-type=application/json')
+  // getJobs() {
+  //   return Promise.all(this.getData('javascript', '1'), this.getData('react', '1'))
+  //   .then(data => console.log('UTILITY', data))
+  //   .catch(error => {
+  //     console.log(error)
+  //   })
+  // },
+
+  getData(search, page) {
+    return fetch(`https://api.adzuna.com/v1/api/jobs/us/search/${page}?app_id=a7e24f78&app_key=2b388189b4767ec893bfecd41687c4e8&results_per_page=20&what=${search}&content-type=application/json`)
     .then(response => response.json())
     .then(jobs => jobs.results.map(job => this.cleanData(job)))
     .catch(error => {
