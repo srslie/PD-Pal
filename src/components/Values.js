9cc6507879b20af378166b3e189431a55cd99eb2
srslie <aliceruppert@gmail.com>
2021-03-05 20:36:48 -0700

    Add props to Values

--- src/components/Values.js
+++ src/components/Values.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 
-export default function Values() {
-  const values = JSON.parse(localStorage.getItem('values'))
+export default function Values({user, values}) {
+  
   return(
     <div className='values'>
       <h2 className='values-title'>Job Values</h2>
       <p className='values-info'>{values ? values : 'What are you working for?'}</p>
-      <Link to='/account'>Update</Link>
+      <Link to='/account'>Update Values</Link>
     </div>
   )
 }
