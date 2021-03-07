f44f513a428fd89b6be1b8fb0058f08941d5ea1d
srslie <aliceruppert@gmail.com>
2021-03-06 17:27:42 -0700

    Add margin  so components can be seen under fixed

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './Account.css';
 
-export default function Account({user, values}) {
+export default function Account({user, values, updateUser, updateValues}) {
+  const userClick = event => {
+    event.preventDefault()
+  }
+  const valuesClick = event => {
+    event.preventDefault()
+    
+  }
   return (
     <div className='account'>
       <h2>{user} Account</h2>
-      <button className='name-button'>Update Name</button>
-      <h3>Values</h3>
+      <input type="text" placeholder="Your Name"></input>
+      <button className='name-button' onClick={userClick}>Update Name</button>
+      <h2>Values</h2>
       <p className='account-values'>{values}</p>
-      <button className='values-button'>Update Values</button>
+      <input type="text" placeholder="Your Name"></input>
+      <button className='values-button' onClick={valuesClick}>Update Values</button>
     </div>
   )
 }
