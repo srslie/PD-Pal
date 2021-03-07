c532f444c25510d89e3fc3d9a961bcb48dda1dd0
srslie <aliceruppert@gmail.com>
2021-03-06 18:34:04 -0700

    Add about styling and p margin

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,7 +6,10 @@ export default function About() {
     <div className="about">
       <h2 className="about-title">About PD-Pal</h2>
       <p className="about-info">
-        This project was coded by <a href="https://github.com/srslie">Alice Ruppert</a> following a <a href="https://turing.io">spec by Turing School Staff</a>. This was the final solo project for Mod 3.
+        This project was coded by <a href="https://github.com/srslie"><button className='alice-link'>Alice Ruppert</button></a> following a <a href="https://turing.io">spec by Turing School Staff</a>. 
+        <br />
+        <br />
+        This was the final solo project for Mod 3.
       </p>
     </div>
   )
