b40c4e0630531ac1f2bcb17d45e2022fe1aae02b
srslie <aliceruppert@gmail.com>
2021-03-06 18:09:04 -0700

    Add margins, remove buttons

--- src/components/JobCard/JobCard.js
+++ src/components/JobCard/JobCard.js
@@ -12,15 +12,11 @@ export default function JobCard({jobs, matchId}) {
     <>
     {jobMatch &&
     <div className='job-card' id={matchId} key={matchId}>
-      <div className="user-interactions">
-        <button className="save-button">Save</button>
-        <button className="applied-button">Mark Applied</button>
-      </div>
-      <h2 className='title'>{title}</h2>
+      <h2 className='job-title'>{title}</h2>
       <a className="company-info" href={url}>{company}</a>
       <p className="location">{location}</p>
-      <Link to={`/job/${matchId}`} className='details-button'>
-        More Details
+        <Link to={`/job/${matchId}`} >
+        <button className='details-button'>More Details</button>
       </Link>
     </div>
     }
--- src/components/JobDetail/JobDetail.js
+++ src/components/JobDetail/JobDetail.js
@@ -6,9 +6,9 @@ export default function JobDetail({matchId, jobs}) {
   const jobMatch = jobs.find(job => job.id === matchId)
   const {company, description, title, location, url} = jobMatch
   return (
-    <>
+    <div className='job-detail'>
     {jobMatch &&
-    <div className='job-card' id={matchId} key={matchId}>
+    <div className='job-card job-detail-card' id={matchId} key={matchId}>
       <h1 className='title'>{title}</h1>
       <a className="company-info" href={url}>{company}</a>
       <p className="location">{location}</p>
@@ -19,7 +19,7 @@ export default function JobDetail({matchId, jobs}) {
       </div>
     </div>
     }
-    </>
+    </div>
   )
 }
 
