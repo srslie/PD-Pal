dea29e6b390a96a28c8c417aaad48476be7f9abc
srslie <aliceruppert@gmail.com>
2021-03-06 18:50:01 -0700

    Add account styling

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -7,17 +7,25 @@ export default function Account({user, values, updateUser, updateValues}) {
   }
   const valuesClick = event => {
     event.preventDefault()
-    
+
   }
   return (
     <div className='account'>
-      <h2>{user} Account</h2>
-      <input type="text" placeholder="Your Name"></input>
-      <button className='name-button' onClick={userClick}>Update Name</button>
-      <h2>Values</h2>
-      <p className='account-values'>{values}</p>
-      <input type="text" placeholder="Your Name"></input>
-      <button className='values-button' onClick={valuesClick}>Update Values</button>
+      <div className='account-name'>
+        <h2>{user} Account</h2>
+        <input className="name-input" type="text" placeholder="Your Name"></input>
+        <button className='name-button' onClick={userClick}>
+          Update Name
+        </button>
+      </div>
+      <div className='account-values'>
+        <h2>Values</h2>
+        <p className='account-values'>{values}</p>
+        <input className="values-input" type="text" placeholder="Values or Mission Statement"></input>
+        <button className='values-button' onClick={valuesClick}>
+          Update Values
+        </button>
+      </div>
     </div>
   )
 }
