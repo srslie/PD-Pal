4013ff4a46645340f26e05c3229570684bcdeede
srslie <aliceruppert@gmail.com>
2021-03-08 20:17:30 -0700

    Update readme and clean up comments

--- src/utility.js
+++ src/utility.js
@@ -1,12 +1,5 @@
 const utility = {
-  // getJobs() { //   return Promise.all(this.getData('javascript', '1'), this.getData('react', '1'))
-  //   .then(data => console.log('UTILITY', data))
-  //   .catch(error => {
-  //     console.log(error)
-  //   })
-  // },
-
-  getData(search, page, setError) {
+  getData(search, page) {
     return fetch(`https://api.adzuna.com/v1/api/jobs/us/search/${page}?app_id=1a060a0c&app_key=4867681585b99bffc654ca5c4768c20c&results_per_page=20&what=${search}&content-type=application/json`)
     .then(response => {
       if (!response.ok) {
